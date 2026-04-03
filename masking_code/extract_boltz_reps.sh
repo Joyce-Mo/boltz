43b2ef324348697718b9d66111fbcb093c941621
Joyce Mo <joycemo413@gmail.com>
2026-04-03 14:05:14 -0700

    Update extract_boltz_reps.sh

--- masking_code/extract_boltz_reps.sh
+++ masking_code/extract_boltz_reps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -cwd
-#$ -o logs/extract_boltz1_reps_$JOB_ID_$date.out
-#$ -e logs/extract_boltz1_reps_$JOB_ID_$date.err
+#$ -o logs/extract_boltz1_reps_$JOB_ID_04032026.out
+#$ -e logs/extract_boltz1_reps_$JOB_ID_04032026.err
 #$ -l mem_free=48G
 #$ -l scratch=2G
 #$ -l h_rt=96:00:00
@@ -22,27 +22,27 @@ REPO_ROOT="/wynton/home/rotation/jqmo/rotation3/boltz"
 PDB_DIR="/wynton/home/rotation/jqmo/rotation3/datasets/cath20-filtered-foldseek"
 
 # Where to save output representations (separate subdirs per model)
-OUTPUT_BASE="/wynton/scratch/jqmo/rotation_datasets/cath20_reps/boltz1_reps"
+OUTPUT_BASE="/wynton/scratch/jqmo/rotation_datasets"
 
 # Checkpoints 
 BOLTZ1_CKPT="${REPO_ROOT}/boltz1_conf.ckpt"
 BOLTZ2_CKPT="${REPO_ROOT}/boltz2_conf.ckpt"
 
 # ---------- run boltz1 ----------
-# echo "boltz1 reps" d
-# python "${REPO_ROOT}/masking_code/extract_cath_reps.py" \
-#     --model_version boltz1 \
-#     --checkpoint "${BOLTZ1_CKPT}" \
-#     --pdb_dir "${PDB_DIR}" \
-#     --save_dir "${OUTPUT_BASE}/boltz1" \
+echo "boltz1 reps" d
+python "${REPO_ROOT}/masking_code/extract_cath_reps.py" \
+    --model_version boltz1 \
+    --checkpoint "${BOLTZ1_CKPT}" \
+    --pdb_dir "${PDB_DIR}" \
+    --save_dir "${OUTPUT_BASE}/boltz1" \
 
 # ---------- run boltz2 ----------
-echo "boltz2 reps"
-python "${REPO_ROOT}/masking_code/extract_cath_reps.py" \
-    --model_version boltz2 \
-    --checkpoint "${BOLTZ2_CKPT}" \
-    --pdb_dir "${PDB_DIR}" \
-    --save_dir "${OUTPUT_BASE}/boltz2" \
+# echo "boltz2 reps"
+# python "${REPO_ROOT}/masking_code/extract_cath_reps.py" \
+#     --model_version boltz2 \
+#     --checkpoint "${BOLTZ2_CKPT}" \
+#     --pdb_dir "${PDB_DIR}" \
+#     --save_dir "${OUTPUT_BASE}/boltz2" \
 
 # ---------- end-of-job ----------
 [[ -n "$JOB_ID" ]] && qstat -j "$JOB_ID"
